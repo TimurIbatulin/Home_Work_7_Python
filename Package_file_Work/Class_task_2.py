4c5b9606e2c5c609d783cd1e50b93fb0fa132540
Тимур Ибатулин <timuribatulin@iMac-Timur-2.Dlink>
2023-08-10 17:54:15 +0300

    Вторая задача

---
+++ Package_file_Work/Class_task_2.py
@@ -0,0 +1,17 @@
+# Напишите функцию, которая генерирует псевдоимена.
+# ✔ Имя должно начинаться с заглавной буквы, состоять из 4-7 букв, среди которых обязательно должны быть гласные.
+# ✔ Полученные имена сохраните в файл.
+
+import random
+from string import ascii_lowercase, ascii_uppercase
+
+
+def give_a_name_file():
+    name_file = random.choice(ascii_uppercase)
+    lenght_name = random.randint(4,6)
+    for i in range(lenght_name):
+        name_file = name_file + random.choice(ascii_lowercase)
+    with open('replacement_fantasy.txt', 'a', encoding='utf-8') as f:
+        f.write(f'{name_file} \n')
+
+give_a_name_file()
