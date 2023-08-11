1958351f3035ffc90b17d912f4342aed93bd874f
Тимур Ибатулин <timuribatulin@iMac-Timur-2.Dlink>
2023-08-11 13:52:27 +0300

    Четвертая и пятая  задачи

--- Package_file_Work/Class_task_4.py
+++ Package_file_Work/Class_task_4.py
@@ -5,4 +5,32 @@
 # ✔ минимальное число случайных байт, записанных в файл, по умолчанию 256
 # ✔ максимальное число случайных байт, записанных в файл, по умолчанию 4096
 # ✔ количество файлов, по умолчанию 42
-# ✔ Имя файла и его размер должны быть в рамках переданного диапазона.
+# ✔ Имя файла и его размер должны быть в рамках переданного диапазон
+
+import random
+from string import ascii_lowercase, ascii_uppercase
+
+
+def auto_create_file(*, extension_file: str, min_len: int, max_len: int, min_byte: int, max_byte: int, quantity_file: int):
+    if min_len == None:
+        min_len = 6
+    if max_len == None:
+        max_len = 30
+    if min_byte == None:
+        min_byte = 256
+    if max_byte == None:
+        max_byte = 4096
+    if quantity_file == None:
+        quantity_file = 42
+    counte = 0
+    while counte < quantity_file:
+        counte += 1
+        name_file = random.choice(ascii_uppercase)
+        lenght_name = random.randint(min_len, max_len)
+        for i in range(lenght_name):
+            name_file = name_file + random.choice(ascii_lowercase)
+        name_file = name_file + random.choice(ascii_lowercase) + '.' + extension_file
+        with open(name_file, 'wb') as f:
+            f.truncate(random.randint(min_byte, max_byte))
+
+
--- Package_file_Work/Class_task_5.py
+++ Package_file_Work/Class_task_5.py
@@ -4,3 +4,23 @@
 # ✔ Количество переданных расширений может быть любым.
 # ✔ Количество файлов для каждого расширения различно.
 # ✔ Внутри используйте вызов функции из прошлой задачи.
+
+
+
+from Class_task_4 import auto_create_file
+
+
+def file_genereid(**kwargs):
+    parametr_extension = locals().get('kwargs')
+    print(parametr_extension)
+    for i in parametr_extension:
+        exten = i
+        count_file = int(parametr_extension.get(i))
+        print(i)
+        print(count_file)
+        auto_create_file(extension_file=exten, min_len=4, max_len=4, min_byte=4, max_byte=4, quantity_file=count_file)
+
+
+
+file_genereid(txt = 1, doc = 2)
+
